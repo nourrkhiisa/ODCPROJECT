e6734073c66fa3628466bd64c640af808464730f
NourSafta <nour.safta@medtech.tn>
2023-04-09 23:29:26 +0100

    LoginScreen-Mobile-app-Updated

--- mobile-app/src/screens/LoginScreen/index.js
+++ mobile-app/src/screens/LoginScreen/index.js
@@ -1,12 +1,17 @@
 import React, { useState, useContext } from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, Text, TextInput, TouchableOpacity,Image } from "react-native";
 import { AuthContext } from "../../contexts/AuthContext";
+import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from "./styles";
+import logo from "../../assets/logo.png";
+
 
 const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [secureTextEntry, setSecureTextEntry] = useState(true);
   const { login } = useContext(AuthContext);
+ 
 
   const handleLogin = async () => {
     try {
@@ -17,28 +22,54 @@ const LoginScreen = ({ navigation }) => {
     }
   };
 
+  const togglePasswordVisibility = () => {
+    setSecureTextEntry(!secureTextEntry);
+  };
+
   return (
     <View style={styles.container}>
+    <Image source={logo} style={styles.logo} />
+      <View style={styles.loginBox}>
       <Text style={styles.title}>Login</Text>
-      <TextInput
-        style={styles.input}
+      
+        <TextInput
         onChangeText={setEmail}
         value={email}
         placeholder="Email"
         keyboardType="email-address"
-      />
-      <TextInput
         style={styles.input}
+        underlineColorAndroid="transparent"
+      
+      />
+      
+      <TextInput
         onChangeText={setPassword}
         value={password}
         placeholder="Password"
-        secureTextEntry
-      />
+        style={styles.input}
+        secureTextEntry={secureTextEntry}
+        underlineColorAndroid="transparent">
+        
+      </TextInput>
+      <Icon
+            name={secureTextEntry ? 'eye-slash' : 'eye'}
+            size={20}
+            color="gray"
+            style={styles.eyeIcon}
+            onPress={togglePasswordVisibility}
+          />
+      
       <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>Login</Text>
+        <Text style={styles.buttonText}>Log in</Text>
       </TouchableOpacity>
+      <TouchableOpacity onPress={() => console.log('Forgot password')}>
+          <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+      </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default LoginScreen;
+
+
--- mobile-app/src/screens/LoginScreen/styles.js
+++ mobile-app/src/screens/LoginScreen/styles.js
@@ -1,37 +1,181 @@
-import { StyleSheet } from "react-native";
+/*import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#FFFFFF",
+    backgroundColor: "#FAF9F6",
   },
   title: {
-    fontSize: 24,
+    fontSize: 27,
     fontWeight: "bold",
-    marginBottom: 20,
+    marginBottom: 25,
   },
   input: {
-    width: "80%",
+    width: "30%",
     height: 40,
     borderColor: "gray",
     borderWidth: 1,
     paddingLeft: 10,
-    marginBottom: 10,
-    borderRadius: 5,
+    marginBottom: 15,
+    borderRadius: 15,
   },
   button: {
-    backgroundColor: "#007BFF",
-    borderRadius: 5,
+    backgroundColor: "#F28C28",
+    borderRadius: 10,
     paddingHorizontal: 20,
     paddingVertical: 10,
     marginTop: 10,
   },
   buttonText: {
     color: "#FFFFFF",
+    fontSize: 17,
     fontWeight: "bold",
   },
 });
 
+export default styles;*/
+
+/*import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FAF9F6',
+  },
+  loginBox: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 10,
+    padding: 25,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    width:500,
+    height:255,
+    shadowRadius: 3.84,
+  },
+  title: {
+    fontSize: 27,
+    fontWeight: 'bold',
+    marginBottom: 25,
+  },
+  input: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingLeft: 10,
+    marginBottom: 15,
+    borderRadius: 15,
+  },
+  inputIcon: {
+    marginRight: 5,
+  },
+  button: {
+    backgroundColor: '#F28C28',
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginTop: 10,
+    width:100,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;*/
+
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FAF9F6',
+  },
+  loginBox: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 20,
+    padding: 35,
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    width: '80%',
+    shadowRadius: 4,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 25,
+    textAlign: 'center',
+  },
+  input: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    height: 45,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingLeft: 15,
+    marginBottom: 15,
+    borderRadius: 20,
+    backgroundColor: '#F8F8F8',
+    shadowColor: '#000',
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    overflow: 'hidden',
+    position: 'relative',
+  },
+  
+  button: {
+    backgroundColor: '#F28C28',
+    borderRadius: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginTop: 10,
+    alignSelf: 'center',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 17,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  forgotPasswordText: {
+    color: '#F28C28',
+    fontSize: 14,
+    marginTop: 5,
+    textAlign: 'center',
+    textDecorationLine: 'underline',
+  },
+  logo: {
+    width: 250,
+    height: 150,
+    marginBottom: 20,
+  },
+  eyeIcon: {
+    position: 'absolute',
+    right: 50,
+    top: '50%',
+    transform: [{ translateY: 8 }],
+    
+  },
+});
+
 export default styles;
+
+
+
